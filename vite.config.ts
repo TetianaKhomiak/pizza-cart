e050e316af74c0169c57c54a0a2c422118309c85
TetianaKhomiak <tkhomiak8889@gmail.com>
2025-05-26 18:46:30 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// @ts-ignore - plugin doesn't have types
-import nodePolyfills from "rollup-plugin-node-polyfills";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pizza-cart/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        // @ts-ignore
-        nodePolyfills(),
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      // add only what you need — keep it minimal
-      stream: "rollup-plugin-node-polyfills/polyfills/stream",
-      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
-    },
-  },
+  base: "/pizza-cart/",
 });
